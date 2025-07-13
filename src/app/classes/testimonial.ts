d53a162b6678b2c51ad7facdcb188858fca9c7c6
Quiza12 <Quiza12@live.com>
2025-07-13 18:32:11 +1000

    Updating testimonials and contacts.

--- src/app/classes/testimonial.ts
+++ src/app/classes/testimonial.ts
@@ -1,12 +1,13 @@
 export class Testimonial {
     title: string;
-    tagline: string;
+    // tagline: string;
     review: string;
   
-    constructor(title: string, tagline: string, review: string) {
+    constructor(title: string, review: string) {
         this.title = title;
-        this.tagline = tagline;
+        // this.tagline = tagline;
         this.review = review;
     }
+
   }
   
--- src/app/components/testimonials/qloans.testimonials.component.ts
+++ src/app/components/testimonials/qloans.testimonials.component.ts
@@ -4,9 +4,9 @@ import { Testimonials } from '../../data/testimonials.service';
 
 
 @Component({
-    selector: 'qloans-app-testimonials',
-    templateUrl: './qloans.testimonials.component.html',
-    styleUrls: ['./qloans.testimonials.component.scss']
+  selector: 'qloans-app-testimonials',
+  templateUrl: './qloans.testimonials.component.html',
+  styleUrls: ['./qloans.testimonials.component.scss']
 })
 
 export class QLoansTestimonialsComponent implements OnInit {
